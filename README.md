9a19c0136ee6fedbae26f555bf2e884c7fd457fa
mannbajpai <mannbajpai.grs@gmail.com>
2024-08-01 12:04:40 +0530

    readme adde

---
+++ README.md
@@ -0,0 +1,29 @@
+# KhaataFlow - Expense Management Application
+
+![KhaataFlow Logo](frontend/src/assets/logo-2.png)
+
+KhaataFlow is an expense management application inspired by Splitwise. It allows users to track personal expenses, manage group expenses, and easily split bills among friends and family. This project uses the PERN stack (PostgreSQL, Express.js, React, Node.js) and is deployed on AWS.
+
+## Table of Contents
+
+- ### [Features](#features)
+- ### [Tech Stack](#tech-stack)
+
+## Features
+
+- **User Authentication**: Secure login and signup with JWT and cookie-parser.
+- **Expense Management**: Add, edit, delete, and track personal expenses.
+- **Group Expenses**: Create groups, manage members, and split expenses using different methods (equal, exact, percentage).
+- **Profile Management**: View and update profile information, including username and profile photo.
+- **Responsive Design**: User-friendly interface built with React and styled with Tailwind CSS and Daisy UI.
+
+## Tech Stack
+
+- **Frontend**: React, Tailwind CSS, Daisy UI
+- **Backend**: Node.js, Express.js
+- **Database**: PostgreSQL
+- **Deployment**: AWS (EC2, RDS, S3, CloudFront)
+- **CI/CD**: GitHub Actions
+
+
+
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,28 @@
 /* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext";
 import fuser1 from "../assets/female-user-1.png"
 import fuser2 from "../assets/female-user-2.png"
 import muser1 from "../assets/male-user-1.png"
 import muser2 from "../assets/male-user-2.png"
-import defaultUser  from "../assets/male-user-2.png"
+import defaultUser from "../assets/male-user-2.png"
 const Navbar = () => {
   const { logout, user } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
   const isHomePage = location.pathname.startsWith("/home/");
   const link = isHomePage ? "/group/" : "/home/";
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
   const handleLogout = () => {
     logout();
-    navigate("/login");
+    navigate("/");
   }
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
 
   return (
     <div className="navbar px-8 rounded-3xl bg-turquoise-green text-primary-content">
@@ -36,12 +42,12 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-center">
-        <Link to="/" className="btn btn-ghost text-2xl">khaataFlow</Link>
+        <Link to="/home" className="btn btn-ghost text-2xl">khaataFlow</Link>
       </div>
       <div className="navbar-end">
         <div className="dropdown dropdown-end">
           {user ? <span className="text-xl font-semibold lg:absolute top-2 right-12">{user.username}</span> : <span>Login please</span>}
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar" onClick={toggleMenu}>
             <div className="w-10 rounded-full">
               <img
                 alt="User Profile"
@@ -50,12 +56,12 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            className={`menu menu-lg dropdown-content font-semibold bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow ${isMenuVisible ? 'visible' : 'hidden'}`}>
             <li>
               <Link to="/me" className="justify-between">
                 Profile
-                <span className="badge">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                <span className="badge glass">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
                   </svg>
                 </span>
@@ -64,8 +70,8 @@ const Navbar = () => {
             <li>
               <Link to="/editProfile" className="justify-between">
                 Settings
-                <span className="badge">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                <span className="badge glass">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
                   </svg>
                 </span>
@@ -74,8 +80,8 @@ const Navbar = () => {
             <li>
               <button className="justify-between" onClick={handleLogout}>
                 Logout
-                <span className="badge">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                <span className="badge glass">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                     <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6Zm-5.03 4.72a.75.75 0 0 0 0 1.06l1.72 1.72H2.25a.75.75 0 0 0 0 1.5h10.94l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 0 0-1.06 0Z" clipRule="evenodd" />
                   </svg>
                 </span>
@@ -84,7 +90,6 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-
     </div>
   )
 }
