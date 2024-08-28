38d598728770bef5382377e18fd3a9bdb7e0dc28
mannbajpai <mannbajpai.grs@gmail.com>
2024-08-28 09:58:13 +0530

    changes to incorporate prod & dev env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,30 @@
 import { Sequelize } from "sequelize"
 import dotenv from "dotenv";
 dotenv.config();
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres",
-        port: process.env.DB_PORT,
-    })
+const sequelize = (process.env.NODE_ENV === 'production') ?
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: "postgres",
+            port: process.env.DB_PORT,
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        }) :
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: "postgres",
+            port: process.env.DB_PORT,
+        })
 
 export default sequelize;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_ENV === "dev" ?
+  import.meta.env.VITE_API_BASE_URL
+  : import.meta.env.VITE_PROD_API_URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL,
   withCredentials: true, // Include cookies with requests
 });
 
